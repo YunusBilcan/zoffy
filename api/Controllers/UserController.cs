47ff14f5f3b849ba346521d26575f22ea0a90155
Yunus Bilcan <yunus.bilcan@gmail.com>
2024-03-17 23:06:25 +0300

    user api yazıldı

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserController(IMongoClient client)
+        {
+            var database = client.GetDatabase("zofy");
+            _users = database.GetCollection<User>("users");
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<User> CreateUser(User user)
+        {
+            await _users.InsertOneAsync(user);
+            return user;
+        }
+
+        [HttpPost("Login")]
+        public async Task<User> Login(User loginRequest)
+        {
+            var user = await _users.Find(u => u.Email == loginRequest.Email && u.Password == loginRequest.Password).FirstOrDefaultAsync();
+            return user;
+        }
+
+        [HttpGet("GetUser")]
+        public async Task<User> GetUser(string email)
+        {
+            var user = await _users.Find(u => u.Email == email).FirstOrDefaultAsync();
+            return user;
+        }
+    }
+
+    public class User
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,23 @@
+using MongoDB.Driver;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+     {
+         options.AddPolicy(name: "AllowAll",
+          builder =>
+          {
+              builder.AllowAnyOrigin() // Tüm kaynaklara izin ver
+                     .AllowAnyMethod() // Tüm metodlara izin ver
+                     .AllowAnyHeader(); // Tüm başlıklara izin ver
+          });
+     });
+
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("MongoDb");
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -9,7 +25,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.MapControllers();
+app.UseStaticFiles();
+app.UseAuthorization();
 app.Run();
