8587f42c1eb8e7b6aa7097e92a66810498e6a8b6
Emre Çiftçi <724nalbur@gmail.com>
2024-03-18 23:09:33 +0300

    Add password visibility and new costumer toggle button

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -8,6 +8,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +24,10 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
           ),
           Positioned(
-            top: MediaQuery.of(context).padding.top, 
+            top: MediaQuery.of(context).padding.top,
             right: 16.0,
             child: IconButton(
-              icon: Icon(Icons.notification_add_sharp, color: Colors.white, size: 30.0), 
+              icon: Icon(Icons.notification_add_sharp, color: Colors.white, size: 30.0),
               onPressed: () {
                 
               },
@@ -46,14 +47,16 @@ class _LoginScreenState extends State<LoginScreen> {
                     color: Color.fromARGB(255, 17, 0, 201),
                   ),
                 ),
-                SizedBox(height: 48.0), 
-                _buildTextField(_usernameController, 'Kullanıcı Adı'),
+                SizedBox(height: 48.0),
+                _buildTextField(_usernameController, 'Kullanıcı Adı'), 
                 SizedBox(height: 16.0),
-                _buildTextField(_passwordController, 'Şifre', isPassword: true),
-                SizedBox(height: 24.0),
+                _buildPasswordTextField(),
+                SizedBox(height: 30.0),
                 _buildLoginButton(),
+                SizedBox(height: 12.0),
+                _buildSignUpButton(), 
                 _buildFlatButton('Şifremi Unuttum', onTap: () {
-                 
+                  
                 }),
                 Spacer(),
               ],
@@ -64,13 +67,12 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-
-  Widget _buildTextField(TextEditingController controller, String label, {bool isPassword = false}) {
+  
+  Widget _buildTextField(TextEditingController controller, String hintText) {
     return TextField(
       controller: controller,
-      obscureText: isPassword,
       decoration: InputDecoration(
-        hintText: label,
+        hintText: hintText,
         filled: true,
         fillColor: Colors.grey[200],
         border: OutlineInputBorder(
@@ -83,32 +85,84 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+  
+  Widget _buildPasswordTextField() {
+    return TextField(
+      controller: _passwordController,
+      obscureText: !_isPasswordVisible,
+      decoration: InputDecoration(
+        hintText: 'Şifre',
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+            color: Color.fromARGB(255, 17, 0, 201),
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        ),
+      ),
+      style: TextStyle(fontSize: 16.0),
+    );
+  }
+
+  
   Widget _buildLoginButton() {
     return ElevatedButton(
       onPressed: () {
-        // 
+        
       },
       child: Text('Giriş Yap'),
       style: ElevatedButton.styleFrom(
         foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 17, 0, 201), 
-        minimumSize: Size(double.infinity, 50), 
+        backgroundColor: Color.fromARGB(255, 17, 0, 201),
+        minimumSize: Size(double.infinity, 50),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0), 
+          borderRadius: BorderRadius.circular(12.0),
         ),
-        elevation: 0,
+        elevation: 10,
         padding: EdgeInsets.symmetric(vertical: 12.0),
       ),
     );
   }
 
+  
+  Widget _buildSignUpButton() {
+    return ElevatedButton(
+      onPressed: () {
+        
+      },
+      child: Text('Müşteri Ol'),
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 17, 0, 201),
+        minimumSize: Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        elevation: 10,
+        padding: EdgeInsets.symmetric(vertical: 12.0),
+      ),
+    );
+  }
+
+  
   Widget _buildFlatButton(String text, {required VoidCallback onTap}) {
     return TextButton(
       onPressed: onTap,
       child: Text(
         text,
         style: TextStyle(
-          color: Color.fromARGB(255, 17, 0, 201), 
+          color: Color.fromARGB(255, 17, 0, 201),
           fontWeight: FontWeight.bold,
         ),
       ),
