d6b1cbf01cb79dc58d16f0d9ac27bd6c696f8c8b
Emre Çiftçi <724nalbur@gmail.com>
2024-03-18 20:39:15 +0300

    Refactor login screen UI and remove unused code

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -1,4 +1,3 @@
-// screens/login_screen.dart
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,85 +6,19 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  bool _rememberMe = false;
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Zoffy'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("lib/assets/login_screen_background.jpg"), 
+            image: AssetImage('lib/assets/login_screen_background.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'E-posta'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Şifre',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.visibility_off),
-                    onPressed: () {
-                     
-                    },
-                  ),
-                ),
-                obscureText: true,
-              ),
-              Row(
-                children: <Widget>[
-                  Checkbox(
-                    value: _rememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _rememberMe = value ?? false;
-                      });
-                    },
-                  ),
-                  Text('Giriş Bilgilerimi Kaydet'),
-                ],
-              ),
-              SizedBox(height: 20), 
-              ElevatedButton(
-                child: Text('Giriş Yap'),
-                onPressed: () {
-                  
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.blue, 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0), 
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15), 
-                ),
-              ),
-              TextButton(
-                child: Text('Hesap Oluştur'),
-                onPressed: () {
-                
-                },
-              ),
-            ],
-          ),
+        child: Center(
         ),
       ),
     );
